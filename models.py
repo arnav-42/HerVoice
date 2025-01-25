5408455aee0e0b8db74c610ab756b70c5dca3f80
Arnav Kalekar <akalekar@purdue.edu>
2025-01-25 17:52:40 -0500

    housekeeping

--- models.py
+++ models.py
@@ -1,9 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+import click
+from flask.cli import with_appcontext
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Bill(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    interests = db.Column(db.String(500), nullable=False)
-    location = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    summary = db.Column(db.Text, nullable=False)
+    interest = db.Column(db.String(255), nullable=True)
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    db.create_all()
+    click.echo('Initialized the database.')
