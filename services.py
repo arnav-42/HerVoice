dda891674b28f5645a61a2a06582ccc991160dcd
Arnav Kalekar <akalekar@purdue.edu>
2025-01-25 17:42:08 -0500

    update congress api call

--- services.py
+++ services.py
@@ -1,9 +1,16 @@
 import requests
+import os
 from models import db, Bill
 
 def fetch_and_classify_bills():
+    c_key = os.getenv("CONGRESS_API_KEY")
+    g_key = os.getenv("GROQ_API_KEY")
     # Fetch recent bills from Congress API
-    response = requests.get("https://api.congress.gov/v3/bills/recent")
+    response = requests.get("https://api.congress.gov/v3/bill")
+    headers = {
+        "Authorization": f"Bearer {c_key}",
+        "Accept": "application/json"
+    }
     bills = response.json().get('bills', [])
 
     for bill in bills:
